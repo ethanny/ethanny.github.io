afbe24867e69dfeddf5ae6a83623b0ca1432315f
ethanny <145535621+ethanny@users.noreply.github.com>
2025-04-17 22:31:46 +0800

    Add files via upload

---
+++ Lab2/script.js
@@ -0,0 +1,20 @@
+
+    const anchor = document.getElementById('anchor');
+    const rect = anchor.getBoundingClientRect();
+    const anchorX = rect.left + rect.width / 2;
+    const anchorY = rect.top + rect.height / 2;
+
+    
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const offsetX = (mouseX - anchorX) * 0.04; 
+    const offsetY = (mouseY - anchorY) * 0.04;
+
+    const faces = document.querySelectorAll('.face');
+faces.forEach(face => {
+  face.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+});
+})
